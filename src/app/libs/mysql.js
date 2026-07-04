f246817285d78f15b9ce1ec621bb811dcbe3a2f6
ErmiasMolla13 <erma13molla@gmail.com>
2026-07-04 08:55:12 +0300

    Update mysql.js

--- src/app/libs/mysql.js
+++ src/app/libs/mysql.js
@@ -1,13 +1,12 @@
-import mysql from 'mysql2/promise'
+import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASS,
-    database:process.env.DB_SCHEMA,
-    waitForConnections:true,
-    connectionLimit: 10,
-    queueLimit: 0
-})
-
-export default pool
+export const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: true 
+  }
+});
